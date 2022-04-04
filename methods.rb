6664d38330ceff89625e1f4bb0561aed5e30cb7d
Allison <amh03160@gmail.com>
2022-04-04 12:16:08 -0500

    commit tree 1a492ce6e2bd22b3c9123b5e97765d17eaea5c6f

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+#greetProgrammer
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+#greet
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+#greetWithDefault
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+#add
+def add(num1,num2)
+    num1 + num2
+end
+
+def halve(number)
+    if(number.class != Integer)
+        return nil
+    end
+    number/2 
+end
+
+def test_out(something)
+    puts(something")
+end
